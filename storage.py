e785702f19564c872edf305b453657117c83e47f
44Schwarz <sirmitya@gmail.com>
2018-09-12 07:41:49 +0300

    Improve key-value storage

--- storage.py
+++ storage.py
@@ -3,20 +3,30 @@ import os
 import tempfile
 import json
 
-storage = dict()
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--val")
 args = parser.parse_args()
-print(args.key, args.val)
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-if args.val is not None:
-    with open(storage_path, 'w') as f:
-        f.write(json.dumps({args.key: args.val}))
+
+if not os.path.exists(storage_path):
+    f = open(storage_path, 'w')
+    f.close()
 else:
     with open(storage_path, 'r') as f:
-        storage = dict()
-        # storage = f.read()
-        print(f.read())
+        try:
+            storage = json.loads(f.read())
+        except:
+            storage = dict()
+
+    if args.val is not None:
+        if storage.get(args.key):
+            storage[args.key].append(args.val)
+        else:
+            storage[args.key] = [args.val]
+
+        with open(storage_path, 'w') as f:
+            f.write(json.dumps(storage))
+    else:
+        print(', '.join(storage.get(args.key, [])))
